54ac6c9de3db8a7154fa4c9c947f6b58d76b2885
ZenAtomsk <ricardo.b3788@gmail.com>
2020-10-08 14:37:40 -0700

    fixed footer and added images, pay no mind to the forms :P

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var hoursOfOperation = ['6am:', '7am:', '8am:', '9am:', '10am:', '11am:', '12pm:
 
 var allCookieStores = [];
 
-var seattle = new CookieStoreExpansion('Seattle', 23, 65, 6.3);
-var tokyo = new CookieStoreExpansion('Tokyo', 3, 24, 1.2);
-var dubai = new CookieStoreExpansion('Dubai', 11, 38, 3.7);
-var paris = new CookieStoreExpansion('Paris', 20, 38, 2.3);
-var lima = new CookieStoreExpansion('Lima', 2, 16, 4.6);
+//will be shared by function and prototype
+var parentElement = document.getElementById('storestable');
+
+
 
 ////Constructor////
 
@@ -26,143 +25,107 @@ function CookieStoreExpansion(storeName, minimumCustomersPerHour, maximumCustome
 
 ////////Prototypes///////
 
-CookieStoreExpansion.prototype.generateCustomersPerHour = function(){
-  for(var i = 0; i < hoursOfOperation.length; i++){
-    var randomNumber = Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1) + this.minimumCustomersPerHour);
-    this.customersPerHour.push(randomNumber);
-  }
-};
-
 
 CookieStoreExpansion.prototype.generateCookiesSoldEachHour = function(){
-  this.generateCustomersPerHour();
   for(var i = 0; i < hoursOfOperation.length; i++){
-    var cookiesSoldThisHour = Math.floor(this.customersPerHour[i] * this.averageCookieSalePerCustomer);
+    var customersForTheHour = generateRandomNumber(this.maximumCustomersPerHour, this.minimumCustomersPerHour);
+    this.customersPerHour.push(customersForTheHour);
+  }
+  for(var j = 0; j < hoursOfOperation.length; j++){
+    var cookiesSoldThisHour = Math.floor(this.customersPerHour[j] * this.averageCookieSalePerCustomer);
     this.totalCookiesForTheDay += cookiesSoldThisHour; //total for each city
     this.cookiesSoldEachHour.push(cookiesSoldThisHour);
   }
-};
+}
 
+//////Helper Function
+
+function generateRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //////Table//////
 
-//will be shared by function and prototype
-var parentElement = document.getElementById('storestable');
 
-
-function generateHeaderRow(){
-  var tableRowElement = document.createElement('tr');
-  var tableHeaderElement = document.createElement('th');
-  tableRowElement.appendChild(tableHeaderElement);
+function headerRow(){
+  var trElement = document.createElement('tr');
 
   //run for loop to put store hours across the top
   for(var i = 0; i < hoursOfOperation.length; i++){
-    var headerHours = document.createElement('th');
-    headerHours.textContent = hoursOfOperation[i];
-    tableRowElement.appendChild(headerHours);
+    var thElement = document.createElement('th');
+    thElement.textContent = hoursOfOperation[i];
+    trElement.appendChild(thElement);
   }
   //after for loop is done, enter the 'total' header cell
-  var storeDailyTotalSold = document.createElement('th');
-  storeDailyTotalSold.textContent = 'Daily Total per Store';
-  tableRowElement.appendChild(storeDailyTotalSold);
+  var thElement2 = document.createElement('th');
+  thElement2.textContent = 'Daily Total per Store';
+  trElement.appendChild(thElement2);
 
-  parentElement.appendChild(tableRowElement);
+  parentElement.appendChild(trElement);
 }
 
-CookieStoreExpansion.prototype.generateRows = function(){
 
-  //creates a tableRow each time the prototype is called
-  var tableRowElement = document.createElement('tr');
+CookieStoreExpansion.prototype.render = function(){
+  var trElement = document.createElement('tr');
+  var thElement = document.createElement('th');
+  thElement.textContent = this.storeName;
+  trElement.appendChild(thElement);
 
-  //calls the values
-  this.generateCookiesSoldEachHour();
+  //make a for and loop over cookies sold each hour
+  for(var i = 0; i < this.cookiesSoldEachHour.length; i++){
 
-  //sticks store name before values for the 5 times the prototype is called, muahahaha
-  this.cookiesSoldEachHour.unshift(this.storeName);
-
-  //this runs the text 15 times (included extra to add name with unshift)
-  for(var i=0; i < this.cookiesSoldEachHour.length; i++) {
-
-    //created table data in the loop
-    var tabledataElement = document.createElement('td');
-
-    //writes that info into each cell (we snuck names in with unshift)
-    tabledataElement.textContent = `${this.cookiesSoldEachHour[i]}`;
-    tableRowElement.appendChild(tabledataElement);
+    var totalCookiesData = document.createElement('td');
+    totalCookiesData.textContent = this.cookiesSoldEachHour[i];
+    trElement.appendChild(totalCookiesData);
   }
-
-  // adds daily totals up
-  var tabledataTwoElement = document.createElement('td');
-  tabledataTwoElement.textContent = `${this.totalCookiesForTheDay}`; //day total for city
-  tableRowElement.appendChild(tabledataTwoElement);
-
-  parentElement.appendChild(tableRowElement);
-
-
+  parentElement.appendChild(trElement);
 };
 
-//add hourly totals
-function createFooter(){
+function generateFooterRow(){
+  var totalOfTotals = 0;
+  var hourlyTotalsArray = [];
 
+  var trElement = document.createElement('tr');
 
-  var parentElement = document.getElementById('stores');
+  var totalPerHour = document.createElement('td');
+  totalPerHour.textContent = 'Total Per Hour';
+  trElement.appendChild(totalPerHour);
 
-  // creates a tableRow for totals
-  var totalsTableRowElement = document.createElement('tr');
-  //creates a tableDataElement for data to enter the row
-  var tableRowDataElement = document.createElement('td');
-  tableRowDataElement.textContent = 'Totals';
-  totalsTableRowElement.appendChild(tableRowDataElement);
-  var hourlyTotal = 0;
+  for(var i = 0; i< hoursOfOperation.length; i++){
 
-  for(var i = 0; i < hoursOfOperation.length; i++){
+    var hourlyCookieSales = 0;
 
+    for(var j = 0; j < allCookieStores.length; j++){
+      hourlyCookieSales += allCookieStores[j].cookiesSoldEachHour[i];
 
-    for(var j = 0; j < allCookieStores[j].length; j++){
-      console.log(j);
-
-      hourlyTotal += allCookieStores[j].cookiesSoldEachHour[i];
+      totalOfTotals += allCookieStores[j].cookiesSoldEachHour[i];
     }
-    //   perHourTotalsAllStores.push(hourlyTotal);
-    // }
+    hourlyTotalsArray.push(hourlyCookieSales);
   }
-  hourlyTotal.unshift('Totals');
-  var tabledataElement = document.createElement('td');
-  tabledataElement.textContent = `${hourlyTotal}`;
-  parentElement.appendChild(tabledataElement);
+
+  console.log('totaloftotals', totalOfTotals);
+  console.log('hourly', hourlyTotalsArray);
+
+  for(var k = 0; k < hourlyTotalsArray.length; k++){
+
+    var tdElement = document.createElement('td');
+    tdElement.textContent = hourlyTotalsArray[k];
+    trElement.appendChild(tdElement);
+  }
+
+  parentElement.appendChild(trElement);
 }
 
-createFooter();
+new CookieStoreExpansion('Seattle', 23, 65, 6.3);
+new CookieStoreExpansion('Tokyo', 3, 24, 1.2);
+new CookieStoreExpansion('Dubai', 11, 38, 3.7);
+new CookieStoreExpansion('Paris', 20, 38, 2.3);
+new CookieStoreExpansion('Lima', 2, 16, 4.6);
 
-generateHeaderRow();
-seattle.generateRows();
-tokyo.generateRows();
-dubai.generateRows();
-paris.generateRows();
-lima.generateRows();
+headerRow();
 
+for(var i = 0; i < allCookieStores.length; i++){
+  allCookieStores[i].generateCookiesSoldEachHour();
+  allCookieStores[i].render();
+}
 
-// SalesLocation.prototype.render = function(){
-//   //make the store name
-//   //create a tr
-//   //create a th
-//   //fill it with content - this.name
-//   // append th to the tr
-
-//   //make a for and loop over cookies sold each hour
-//     //make a td
-//     //fill it with content - data in cookies array[i]
-// }
-
-// function createFooter(){
-
-
-//   var tableRowTotalsElement = document.createElement('tr');
-//   tableRowTotalsElement.textContent = 'totals';
-//   parentElement.appendChild(tableRowTotalsElement);
-
-//   for(var a = 0; a < .totalCookiesForTheDay.length; a++){
-//     for(var b = 0; b < this.cookiesSoldThisHour.length; b++){
-
-//     }
-//   }
-// }
+generateFooterRow();
